d2719692c6fc8037fc59bdd18d0e0ff20c2d4599
Reyan Shah <reyanshah10c@gmail.com>
2025-06-15 22:56:41 +0530

    fixed .env issue

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   if (kIsWeb) {
     // Web requires explicit options
     await Firebase.initializeApp(
@@ -16,10 +17,10 @@ void main() async {
       options: FirebaseOptions(
       apiKey: dotenv.env['FIREBASE_API_KEY']!,
       authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
       projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
       storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
       messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-      appId: dotenv.env['FIREBASE_APP_ID']!,
       measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
     ),
     );
